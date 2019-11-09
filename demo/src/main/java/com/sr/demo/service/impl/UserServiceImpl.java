73e8d16965c4e7fb41aaf04e8078b1d6431f85df
石金普 <shi.jinpu@iwhalecloud.com>
2019-11-09 19:49:21 +0800

    add(user): 用户注册+密码密文

--- demo/src/main/java/com/sr/demo/service/impl/UserServiceImpl.java
+++ demo/src/main/java/com/sr/demo/service/impl/UserServiceImpl.java
@@ -1,10 +1,19 @@
 package com.sr.demo.service.impl;
 
+import com.sr.demo.common.SrErrorEnum;
+import com.sr.demo.common.SrException;
 import com.sr.demo.mapper.UserModelMapper;
 import com.sr.demo.model.UserModel;
 import com.sr.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.stereotype.Service;
+import sun.misc.BASE64Encoder;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Date;
 
 /**
  * @author pushkin
@@ -24,5 +33,24 @@ public class UserServiceImpl implements UserService {
     @Override
     public UserModel getUser(Integer id) {
         return userModelMapper.selectByPrimaryKey(id);
+    }
+
+    @Override
+    public UserModel register(UserModel registerUser) throws SrException {
+        registerUser.setCreatedAt(new Date());
+        registerUser.setUpdatedAt(new Date());
+        try{
+            userModelMapper.insertSelective(registerUser);
+        } catch (DuplicateKeyException){
+            throw new SrException(SrErrorEnum.REGISTER_DUP_FAIL);
+        }
+        return getUser(registerUser.getId());
+    }
+
+    private String encodeByMd5(String str) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        // 确认计算方法MD5
+        MessageDigest messageDigest = MessageDigest.getInstance("MD5");
+        BASE64Encoder base64Encoder = new BASE64Encoder();
+        return base64Encoder.encode(messageDigest.digest(str.getBytes("utf-8")));
     }
 }
