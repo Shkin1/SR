f9c719a815bfe12089a29a9b2b5c87971881516a
石金普 <shi.jinpu@iwhalecloud.com>
2019-11-10 22:56:54 +0800

    feat: 搜索v1.0, 推荐v1.0

---
+++ demo/src/main/java/com/sr/demo/controller/ShopController.java
@@ -0,0 +1,73 @@
+package com.sr.demo.controller;
+
+import com.sr.demo.common.CommonRes;
+import com.sr.demo.common.SrErrorEnum;
+import com.sr.demo.common.SrException;
+import com.sr.demo.model.CategoryModel;
+import com.sr.demo.model.ShopModel;
+import com.sr.demo.service.CategoryService;
+import com.sr.demo.service.ShopService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.math.BigDecimal;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Controller("/shop")
+@RequestMapping("/shop")
+public class ShopController {
+
+    @Autowired
+    private ShopService shopService;
+
+    @Autowired
+    private CategoryService categoryService;
+
+    // 推荐服务V1.0
+    @RequestMapping("/recommend")
+    @ResponseBody
+    public CommonRes recommend(@RequestParam(name="longitude")BigDecimal longitude,
+                               @RequestParam(name="latitude")BigDecimal latitude) throws SrException {
+        if(longitude == null || latitude == null){
+            throw new SrException(SrErrorEnum.PARAMETER_VALIDATION_ERROR);
+        }
+
+        List<ShopModel> shopModelList = shopService.recommend(longitude,latitude);
+        return CommonRes.create(shopModelList);
+    }
+
+
+    // 搜索服务V1.0
+    @RequestMapping("/search")
+    @ResponseBody
+    public CommonRes search(@RequestParam(name="longitude")BigDecimal longitude,
+                            @RequestParam(name="latitude")BigDecimal latitude,
+                            @RequestParam(name="keyword")String keyword,
+                            @RequestParam(name="orderby",required = false)Integer orderby,
+                            @RequestParam(name="categoryId",required = false)Integer categoryId,
+                            @RequestParam(name="tags",required = false)String tags) throws SrException {
+        if(StringUtils.isEmpty(keyword) || longitude == null || latitude == null){
+            throw new SrException(SrErrorEnum.PARAMETER_VALIDATION_ERROR);
+        }
+
+        List<ShopModel> shopModelList = shopService.search(longitude,latitude,keyword,orderby,categoryId,tags);
+        List<CategoryModel> categoryModelList = categoryService.selectAll();
+        List<Map<String,Object>> tagsAggregation = shopService.searchGroupByTags(keyword,categoryId,tags);
+        Map<String,Object> resMap = new HashMap<>();
+        resMap.put("shop",shopModelList);
+        resMap.put("category",categoryModelList);
+        resMap.put("tags",tagsAggregation);
+        return CommonRes.create(resMap);
+
+    }
+
+
+
+
+}
